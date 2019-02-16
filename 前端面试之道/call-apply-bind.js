be7bb339751846ccd7e0d9d4ae363d91395883e7
zkj <whutzkj@163.com>
2019-02-16 14:39:21 +0800

    update

---
+++ 前端面试之道/call-apply-bind.js
@@ -0,0 +1,48 @@
+// call apply bind函数实现方式
+
+Function.prototype.myCall = function(context, ...rest)  {
+  context = context || window
+  context.fn = this
+  const result = context.fn(...rest)
+  Reflect.deleteProperty(context, 'fn')
+  return result
+}
+
+
+Function.prototype.myApply = function(context, rest)  {
+
+  context = context || window
+  context.fn = this
+  const result = rest ? context.fn(...rest) : context.fn()
+  Reflect.deleteProperty(context, 'fn')
+  return result
+}
+
+Function.prototype.myBind = function(context, ...rest) {
+  
+  context = context || window
+  const _this = this
+  // 返回一个函数
+  return function Fn() {
+    // 因为返回一个函数，我们可以 new F(), 所以需要判断
+    if(this instanceof Fn){
+      return new _this(...rest, arguments)
+    }else{
+      return _this.apply(context, rest.concat(...arguments))
+    }
+  }
+}
+
+function Food(name, price){
+  this.name = name
+  this.price = price
+}
+
+function Cake(name, price){
+  // Food.call(this, name, price)
+  // Food.myCall(this, name, price)
+  Food.myBind(this)(name, price)
+  this.taste = 'sweet'
+}
+
+var cake = new Cake('黑森林', 20)
