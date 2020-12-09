260adc542e0e32f883ecb1fe2662b57e8ef1bfab
zkj <kj.zhu@trip.com>
2020-12-09 14:29:24 +0800

    kmp

--- 算法/KMP/index.js
+++ 算法/KMP/index.js
@@ -1,24 +1,51 @@
-function indexOf(origin, target) {
-    let pos = -1;
-    let count = 0
-    for (let i = 0; i < origin.length; i++) {
-        if(pos === -1){
-            let temp = []
-            for (let j = 0; j < target.length; j++) {
-                count++
-                if(origin[i+j] === target[j]){
-                    temp.push(true)
-                }else{
-                    break;
-                }
-            }
-            if(temp.filter(_ => _).length === target.length){
-                pos = i;
-            }
-            
+// 字符串朴素模式匹配法
+function match(source, target) {
+    let i = 0, j = 0;
+    while (i < source.length && j < target.length) {
+        if (source[i] === target[j]) {
+            i++;
+            j++;
+        } else {
+            i = i - j + 1
+            j = 0;
         }
-
+        if (j === target.length) return i - j
     }
-    console.log('执行次数',count)
-    return pos;
+    return -1
 }
+console.log(match('qeeabacbacdef', 'abac'))
+
+// kmp算法优化
+function getNext(str) {
+    let next = [];
+    next[0] = -1;
+    let i = 0, j = -1;
+    while (i < str.length - 1) {
+        if (j === -1 || str[i] === str[j]) {
+            ++i;
+            ++j;
+            next[i] = j;
+        } else {
+            j = next[j]
+        }
+    }
+    return next
+}
+
+let next = getNext('abababca')
+
+function kmp(source, target) {
+    let i = 0, j = 0;
+    while (i < source.length && j < target.length) {
+        if (j == -1 || source[i] === target[j]) {
+            i++;
+            j++;
+        } else {
+            j = next[j]
+        }
+    }
+    if (j === target.length) return i - j
+    return -1
+}
+
+console.log(kmp('ababababca', 'abababca'))
