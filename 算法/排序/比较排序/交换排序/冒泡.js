cbdeecd57db8a04197d49900d3359163077a92f4
zkj <kj.zhu@trip.com>
2021-06-17 13:21:57 +0800

    update

--- 算法/排序/比较排序/交换排序/冒泡.js
+++ 算法/排序/比较排序/交换排序/冒泡.js
@@ -1,7 +1,9 @@
 function bubbleSort(arr){
     const len = arr.length;
+    let count = 0;
     for(let i = 0; i < len - 1; i++){
         for(let j = 0; j < len - 1 - i; j++){
+            count++;
             if(arr[j] > arr[j+1]){
                 const temp = arr[j+1];
                 arr[j+1] = arr[j];
@@ -9,7 +11,31 @@ function bubbleSort(arr){
             }
         }
     }
+    console.log('执行了'+ count + '次')
     return arr
 }
 
-console.log(bubbleSort([34,5,6,7,8,12,23,1]))
+console.log(bubbleSort([4,3,2,1]))
+
+// 改进型冒泡
+// 虽然时间复杂度依然是n2，但是可以减少循环次数
+function bubbleSort2(arr){
+    let i = arr.length - 1;
+    let count = 0;
+    while(i > 0){
+        let pos = 0;
+        for(let j = 0; j < i; j++){
+            count++;
+            if(arr[j] > arr[j+1]){
+                pos = j;
+                const temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        i = pos;
+    }
+    console.log('执行了'+ count + '次')
+    return arr;
+}
+console.log(bubbleSort2([4,3,2,1]))
