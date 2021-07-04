6910797d447feba6b8126ea95d044527b90205b9
kj.zhu <kj.zhu@ctrip.com>
2021-07-04 23:03:07 +0800

    update

--- js基础/函数柯里化.js
+++ js基础/函数柯里化.js
@@ -1,7 +1,7 @@
 
 // 柯里化（Currying）是把接受多个参数的函数变换成接受一个单一参数(最初函数的第一个参数)的函数，并且返回接受余下的参数且返回结果的新函数的技术
-function add(...args){
-    return args.reduce((pre, current) => pre + current, 0)
+function add(x,y,z,a){
+    return [x,y,z,a].reduce((pre, current) => pre + current, 0)
 }
 // 经过柯里化后返回的函数应该是这个
 // function curry(x){
@@ -12,24 +12,37 @@ function add(...args){
 // curry(add)
 
 
-function curry(fn, length){
-    // if(fn.length === 0){
-    //     return fn
-    // }
+// function curry(fn, length){
+//     // if(fn.length === 0){
+//     //     return fn
+//     // }
 
-    function _curried(depth, ...args){
-        return function(newArgument){
-            if(depth === 1){
-                return fn(...args, newArgument)
+//     function _curried(depth, ...args){
+//         return function(newArgument){
+//             if(depth === 1){
+//                 return fn(...args, newArgument)
+//             }
+//             return _curried(depth-1, ...args, newArgument)
+//         }
+//     }
+
+//     return _curried(length)
+// }
+
+function curry(fn){
+    function _curry(...args){
+        if(args.length >= fn.length){
+            return fn(...args)
+        }else{
+            return function(...arg){
+                return _curry(...args,...arg)
             }
-            return _curried(depth-1, ...args, newArgument)
         }
     }
-
-    return _curried(length)
+    return _curry
 }
 
-test = curry(add, 4)
+test = curry(add)
 console.log(test(3)(4)(5)(6))
 
 
